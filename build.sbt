4c7bf1e8365eda0e978abe6d3e1bfa317b1e30fd
Karol Czulkowski <karol.czulkowski@intel.com>
2016-12-12 23:28:57 +0100

    After merge

--- build.sbt
+++ build.sbt
@@ -10,5 +10,6 @@ resolvers ++= Seq(
 )
 
 libraryDependencies ++= Seq(
-  "com.chuusai" %% "shapeless" % "2.3.2"
+  "com.chuusai" %% "shapeless" % "2.3.2",
+  "org.scala-lang" % "scala-reflect" % "2.12.1"
 )
--- src/main/scala/com/github/kczulko/Main.scala
+++ src/main/scala/com/github/kczulko/Main.scala
@@ -1,12 +1,17 @@
 package com.github.kczulko
 
 import shapeless.{HList, ::, HNil}
-
+import shapeless.syntax.singleton._
+import scala.reflect.runtime.universe._
 
 object Main {
   def main(args: Array[String]): Unit = {
     val product: String :: HNil = "Sunday" :: HNil
-//    println { product }
     val list = 1 :: true :: Nil
+
+    var x  = 42.narrow
+    val another: Int = 42
+    println(reify(x))
+    println(reify(another))
   }
 }
